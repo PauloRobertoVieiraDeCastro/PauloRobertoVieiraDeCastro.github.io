9b44699b10ea6150ad6c9d83aaa17fadd66e3408
Paulo Roberto Vieira de Castro <44949683+PauloRobertoVieiraDeCastro@users.noreply.github.com>
2021-02-17 14:57:09 -0300

    Update vendas.js

--- get_festas/vendas.js
+++ get_festas/vendas.js
@@ -13,7 +13,7 @@ function calcular(){
 	if (confirm("Você deseja enviar esses valores via WhatsApp?")) {
 		let number = '5521974971784'
 		// o texto ou algo vindo de um <textarea> ou <input> por exemplo
-		let msg = 'Olá a todas. O valor para a GET é de: R$'+parseFloat(get_festas.toFixed(2))+', para Gi é: R$'+parseFloat(v_gi.toFixed(2))+', Neusa é: R$'+ parseFloat(v_neusa.toFixed(2))+' e Thata é: R$' + parseFloat(v_tha.toFixed(2))
+		let msg = 'Olá a todas. O valor para a GET é de: R$ '+parseFloat(get_festas.toFixed(2))+', para Gi é: R$ '+parseFloat(v_gi.toFixed(2))+', Neusa é: R$ '+ parseFloat(v_neusa.toFixed(2))+' e Thata é: R$' + parseFloat(v_tha.toFixed(2))
 		window.open("https://api.whatsapp.com/send?phone=5521974971784&text=%20"+msg);
 		//window.open("https://api.whatsapp.com/send?phone=5521974971784&text=sua%20"+msg);	
 	} 
