5778177c1a6d29090831ad4aea5cd7d0f80d78c3
Paulo Roberto Vieira de Castro <44949683+PauloRobertoVieiraDeCastro@users.noreply.github.com>
2021-06-19 19:50:35 -0300

    Add files via upload

---
+++ getfest/js/slide.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+  $("#flip").hover(function(){
+    $("#panel").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip2").hover(function(){
+    $("#panel2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip3").hover(function(){
+    $("#panel3").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip4").hover(function(){
+    $("#panel4").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip5").hover(function(){
+    $("#panel5").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip6").hover(function(){
+    $("#panel6").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip7").hover(function(){
+    $("#panel7").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip8").hover(function(){
+    $("#panel8").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip9").hover(function(){
+    $("#panel9").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip10").hover(function(){
+    $("#panel10").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip11").hover(function(){
+    $("#panel11").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip12").hover(function(){
+    $("#panel12").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip13").hover(function(){
+    $("#panel13").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip14").hover(function(){
+    $("#panel14").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#flip15").hover(function(){
+    $("#panel15").slideToggle("slow");
+  });
+});
