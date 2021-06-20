9b9268e2bbbe6df5b2d1afe7482c19f3148b2f4c
Paulo Roberto Vieira de Castro <44949683+PauloRobertoVieiraDeCastro@users.noreply.github.com>
2021-06-20 20:43:48 -0300

    Add files via upload

---
+++ getfest/js/open.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    contador = 2000;
+    a = $("#teste");
+    a.fadeIn(contador);
+
+    /*$.each([a,b,c], function() {
+        $(this).fadeIn(contador);
+        contador +=2000
+    });*/
+})
+
