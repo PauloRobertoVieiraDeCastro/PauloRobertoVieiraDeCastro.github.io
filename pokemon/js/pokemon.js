2c9b941bdae8be12fe17dbfd2ccf62336961b715
Paulo Roberto Vieira de Castro <44949683+PauloRobertoVieiraDeCastro@users.noreply.github.com>
2021-09-25 11:18:34 -0300

    Update pokemon.js

--- pokemon/js/pokemon.js
+++ pokemon/js/pokemon.js
@@ -1,8 +1,11 @@
 var n = 898;
-var lista = []
+var lista = [];
+var lista_tipo = []
 $(document).ready(function (){
 	var pokemon
 	const promises = [];
+	document.querySelector("#modelo").value = "Selecione um tipo";
+	document.querySelector("#pokemongo").value = " ";
 	 //numero de pokemons a visualizar
 	for(var i=1;i<=n;i++){
 		var url = "https://pokeapi.co/api/v2/pokemon/"+i;
@@ -11,11 +14,11 @@ $(document).ready(function (){
 	Promise.all(promises).then((results) => {
 		//console.log(results[0].abilities)
 		//console.log(results[0].abilities.map((type) => type.ability.name).join('/'))
-		console.log(results)
+		
         	var pokemon = results.map((result) => ({
             name: result.name,
             image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join('/'),
+            type: result.types.map((type) => type.type.name).join('-'),
             id: result.id,
             stats0: result.stats[0]['base_stat'],
             stats1: result.stats[1]['base_stat'],
@@ -23,7 +26,7 @@ $(document).ready(function (){
             stats3: result.stats[3]['base_stat'],
             stats4: result.stats[4]['base_stat'],
             stats5: result.stats[5]['base_stat'],
-			ability: result.abilities.map((type) => type.ability.name).join('/')
+			ability: result.abilities.map((type) => type.ability.name).join('-')
         }));
         j = 0;
 	var cria_div;
@@ -47,7 +50,9 @@ $(document).ready(function (){
 		cria_div_p.classList.add("my-2");
 		cria_div_p.classList.add("coluna"+ii+"-"+j+"a");
 		cria_div_p.classList.add("a"+pokemon[ii]['name']);
+		cria_div_p.classList.add("a"+pokemon[ii]['type']+"a");
 		lista.push(pokemon[ii]['name'])
+		lista_tipo.push(pokemon[ii]['type'])
 		document.getElementById("linha"+j+"a").appendChild(cria_div_p);
 
 		var cria_div_card = document.createElement('div');
@@ -69,10 +74,81 @@ $(document).ready(function (){
 		pp.classList.add("px-1");
 		pp.style = " font-size: 1.1rem; font-weight: bold"
 		pp.innerHTML = "Type: "+ pokemon[ii]['type'];
+		var cor;
+
+		if(pokemon[ii]['type'].includes("grass")){
+			cor="green";
+			back = "white"
+		}else if(pokemon[ii]['type'].includes("fire")){
+			cor="#EE8130"
+			back = "white"
+		}else if(pokemon[ii]['type'].includes("water")){
+			cor="blue"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("electric")){
+			cor="#F7D02C"
+			back = "black"
+		}
+		else if(pokemon[ii]['type'].includes("normal")){
+			cor="#A8A77A"
+			back = "black"
+		}
+		else if(pokemon[ii]['type'].includes("ice")){
+			cor="#96D9D6"
+			back = "black"
+		}
+		else if(pokemon[ii]['type'].includes("fighting")){
+			cor="#C22E28"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("poison")){
+			cor="rebeccapurple"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("ground")){
+			cor="#E2BF65"
+			back = "black"
+		}
+		else if(pokemon[ii]['type'].includes("bug")){
+			cor="#98FB98"
+			back = "black"
+		}
+		else if(pokemon[ii]['type'].includes("dragon")){
+			cor="#6F35FC"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("flying")){
+			cor="#A98FF3"
+			back = "black"
+		}
+		else if(pokemon[ii]['type'].includes("rock")){
+			cor="#B87333"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("psychic")){
+			cor="#F95587"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("ghost")){
+			cor="#735797"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("dark")){
+			cor="#705746"
+			back = "white"
+		}
+		else if(pokemon[ii]['type'].includes("steel")){
+			cor="#B7B7CE"
+			back = "white"
+		}else{
+			cor="#D685AD"
+			back = "white"
+		}
 
 		var pp2 = document.createElement('p');
 		pp2.classList.add("px-1");
-		pp2.style = "font-size: 1.1rem; font-weight: bold"
+		pp2.style = "font-size: 0.9rem; font-weight: bold"
 		pp2.innerHTML = "Ability: "+ pokemon[ii]['ability'];
 
 
@@ -85,44 +161,44 @@ $(document).ready(function (){
 		var cria_div_card_title = document.createElement('div');
 		cria_div_card_title.classList.add("card-body");
 		cria_div_card_title.classList.add("carda"+ii+"-"+j+"a");
-		cria_div_card_title.style = "background: blue;border-radius:0 0 15px 15px; margin-top:-5px"		
+		cria_div_card_title.style = "background:"+cor+";border-radius:0 0 15px 15px; margin-top:-5px; color:"+back;		
 		document.querySelector(".card"+ii+"-"+j+"a").appendChild(cria_div_card_title);
 
 		//texto no corpo
 		var cria_h5 = document.createElement('h5');
 		cria_h5.classList.add("card-title");
 		cria_h5.classList.add("text-center");
-		cria_h5.style = "text-transform: capitalize; font-size: 1.8rem; color: white"
+		cria_h5.style = "text-transform: capitalize; font-size: 1.8rem; color: "+back
 		cria_h5.innerHTML =  ii+1 +". " + pokemon[ii]['name'];
 		document.querySelector(".carda"+ii+"-"+j+"a").appendChild(cria_h5);
 
 		var cria_paragrafo = document.createElement('p');
 		cria_paragrafo.classList.add("text-center");
-		cria_paragrafo.style = "text-transform: capitalize; color: white;"
+		cria_paragrafo.style = "text-transform: capitalize; color: "+back
 
 		var cria_paragrafo2 = document.createElement('p');
 		cria_paragrafo2.classList.add("text-center");
-		cria_paragrafo2.style = "color: white;"
+		cria_paragrafo2.style = "color: "+back
 
 		var cria_paragrafo3 = document.createElement('p');
 		cria_paragrafo3.classList.add("text-center");
-		cria_paragrafo3.style = "color: white;"
+		cria_paragrafo3.style = "color: "+back
 
 		var cria_paragrafo4 = document.createElement('p');
 		cria_paragrafo4.classList.add("text-center");
-		cria_paragrafo4.style = "color: white;"
+		cria_paragrafo4.style = "color: "+back
 
 		var cria_paragrafo5 = document.createElement('p');
 		cria_paragrafo5.classList.add("text-center");
-		cria_paragrafo5.style = "color: white;"
+		cria_paragrafo5.style = "color: "+back
 
 		var cria_paragrafo6 = document.createElement('p');
 		cria_paragrafo6.classList.add("text-center");
-		cria_paragrafo6.style = "color: white;"
+		cria_paragrafo6.style = "color: "+back
 
 		var cria_paragrafo7 = document.createElement('p');
 		cria_paragrafo7.classList.add("text-center");
-		cria_paragrafo7.style = "color: white;"
+		cria_paragrafo7.style = "color: "+back
 
 		//cria_paragrafo.innerHTML =  "Type: "+ pokemon[ii]['type'];
 		cria_paragrafo2.innerHTML =  "HP: "+ pokemon[ii]['stats0'];
@@ -150,12 +226,34 @@ $(document).ready(function (){
 	
 });
 
+function filtro(){
+	var tipo = document.getElementById("modelo").value;
+	if(tipo != "Selecione um tipo"){
+		document.getElementById("pokemongo").disabled = true;
+		document.getElementById("pokemongo").value = " ";
+		for(let i=0;i<n;i++){
+			//if(lista_tipo[i].toLowerCase().includes(tipo.toLowerCase())){
+			if((document.querySelector(".a"+lista[i].toLowerCase()).innerText.includes(tipo.toLowerCase()))){
+				document.querySelector(".a"+lista[i].toLowerCase()).style.display="block";
+			}else{
+				document.querySelector(".a"+lista[i].toLowerCase()).style.display="none";
+			}
+		}
+	}else{
+		document.getElementById("pokemongo").value = " ";
+		document.getElementById("pokemongo").disabled = false;
+		for(var i=0;i<n;i++){
+			document.querySelector(".a"+lista[i].toLowerCase()).style.display="block";
+		}
+	}
+}
+
 function autocompletado(){
 	var pok = document.getElementById("pokemongo").value;
 	var tam = pok.length;
 	var tela = document.querySelector(".a"+pok.toLowerCase());
 	if(tela != null){
-		for(var i=0;i<n;i++){
+		for(let i=0;i<n;i++){
 			if(lista[i].toLowerCase() == pok.toLowerCase()){
 				document.querySelector(".a"+lista[i].toLowerCase()).style.display="block"
 			}else{
@@ -183,8 +281,6 @@ function autocompletado(){
 		divm4.classList.add('modal-body');
 		divm4.innerHTML="cloroquina"
 		document.querySelector(".modal-content").appendChild(divm4);*/
-
-
 	}else{
 		for(var i=0;i<n;i++){
 			document.querySelector(".a"+lista[i].toLowerCase()).style.display="block";
@@ -192,6 +288,9 @@ function autocompletado(){
 	}
 	
 }
+
+
+
 
 
 
