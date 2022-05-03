9ab1f0042351cdf76db1d9c9d5310057fd0c058e
Paulo Roberto Vieira de Castro <vieira.castro.1987@gmail.com>
2022-05-03 09:43:39 -0300

    Create labp.js

---
+++ labp_design/js/labp.js
@@ -0,0 +1,81 @@
+function clicar_mvd100(){
+	var x = document.getElementById('status_mvd100');
+	if(x.value==0){
+		x.style = "background: green; color: white; font-weight: bold";
+	}
+	if(x.value==1){
+		x.style = "background: yellow; color: black; font-weight: bold";
+	}
+	if(x.value==2){
+		x.style = "background: red; color: white; font-weight: bold";	
+	}
+}
+
+function clicar_mvd30(){
+	var x = document.getElementById('status_mvd30');
+	if(x.value==0){
+		x.style = "background: green; color: white; font-weight: bold";
+	}
+	if(x.value==1){
+		x.style = "background: yellow; color: black; font-weight: bold";
+	}
+	if(x.value==2){
+		x.style = "background: red; color: white; font-weight: bold";	
+	}
+}
+
+function clicar_minie(){
+	var x = document.getElementById('status_minie');
+	if(x.value==0){
+		x.style = "background: green; color: white; font-weight: bold";
+	}
+	if(x.value==1){
+		x.style = "background: yellow; color: black; font-weight: bold";
+	}
+	if(x.value==2){
+		x.style = "background: red; color: white; font-weight: bold";	
+	}
+}
+
+function clicar_pot1(){
+	var x = document.getElementById('status_pot1');
+	if(x.value==0){
+		x.style = "background: green; color: white; font-weight: bold";
+	}
+	if(x.value==1){
+		x.style = "background: yellow; color: black; font-weight: bold";
+	}
+	if(x.value==2){
+		x.style = "background: red; color: white; font-weight: bold";	
+	}
+}
+
+function clicar_pot2(){
+	var x = document.getElementById('status_pot2');
+	if(x.value==0){
+		x.style = "background: green; color: white; font-weight: bold";
+	}
+	if(x.value==1){
+		x.style = "background: yellow; color: black; font-weight: bold";
+	}
+	if(x.value==2){
+		x.style = "background: red; color: white; font-weight: bold";	
+	}
+}
+
+function clicar_lub(){
+	var x = document.getElementById('status_lub');
+	if(x.value==0){
+		x.style = "background: green; color: white; font-weight: bold";
+	}
+	if(x.value==1){
+		x.style = "background: yellow; color: black; font-weight: bold";
+	}
+	if(x.value==2){
+		x.style = "background: red; color: white; font-weight: bold";	
+	}
+}
+
+
+
+
